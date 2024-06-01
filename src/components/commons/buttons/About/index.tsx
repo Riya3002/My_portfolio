4625579578ea40e845e550e77ac89c380cbcb4f7
Riya <riyabansal2103@gmail.com>
2024-06-01 17:31:37 +0530

    updated link to about

--- src/components/commons/buttons/About/index.tsx
+++ src/components/commons/buttons/About/index.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Aboutbtn(
 ){
     return(
-        <Link  href="../about" >          
+        <Link  href="../About" >          
                 <div className={styles.btn}>
                 <FaUserAstronaut className={styles.icon}/>
             <p className={styles.text}>About me</p>
