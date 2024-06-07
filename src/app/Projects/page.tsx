cc4a5e4b2d6f7f31e334cee76205969b97faae41
Riya <riyabansal2103@gmail.com>
2024-06-07 23:25:13 +0530

    updated projects

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Card from "@/components/cards/webcard";
 import ECard from "@/components/cards/embedded";
-import Work from "@/components/Work";
 const Projects =()=>{
     return(
         <div className=" text-white " style={{backgroundImage:`url('/bg_project.jpeg')`, backgroundRepeat: "no-repeat", backgroundSize:'cover'}}>
-            <div>
-                <Work/>
-            </div>
             <div>
                 <div className=" flex justify-center text-[30px] py-8 underline">Website Development Projects and Contributions</div>
                 <div className="flex row justify-evenly p-8">
